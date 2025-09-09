5eba53266f4fe6c13ab24bcdc122df78cd2f5cd5
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-09-10 00:46:58 +0900

    refactor: DnD 시스템 개선

--- src/ScheduleTables.tsx
+++ src/ScheduleTables.tsx
@@ -3,7 +3,83 @@ import ScheduleTable from "./ScheduleTable.tsx";
 import { useScheduleContext } from "./ScheduleContext.tsx";
 import SearchDialog from "./SearchDialog.tsx";
 import { useState, useCallback, memo } from "react";
+import ScheduleDndProvider from "./ScheduleDndProvider.tsx";
+import { Schedule } from "./types.ts";
 
+export const ScheduleTableWrapper = memo(
+  ({
+    tableId,
+    schedules,
+    index,
+    setSearchInfo,
+    duplicate,
+    disabledRemoveButton,
+    remove,
+    handleScheduleTimeClick,
+    handleDeleteButtonClick,
+  }: {
+    tableId: string;
+    schedules: Schedule[];
+    index: number;
+    setSearchInfo: (info: { tableId: string }) => void;
+    duplicate: (targetId: string) => void;
+    disabledRemoveButton: boolean;
+    remove: (targetId: string) => void;
+    handleScheduleTimeClick: (
+      tableId: string,
+      timeInfo: { day: string; time: number }
+    ) => void;
+    handleDeleteButtonClick: (
+      tableId: string,
+      timeInfo: { day: string; time: number }
+    ) => void;
+  }) => {
+    return (
+      <Stack key={tableId} width="600px">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Heading as="h3" fontSize="lg">
+            시간표 {index + 1}
+          </Heading>
+          <ButtonGroup size="sm" isAttached>
+            <Button
+              colorScheme="green"
+              onClick={() => setSearchInfo({ tableId })}
+            >
+              시간표 추가
+            </Button>
+            <Button
+              colorScheme="green"
+              mx="1px"
+              onClick={() => duplicate(tableId)}
+            >
+              복제
+            </Button>
+            <Button
+              colorScheme="green"
+              isDisabled={disabledRemoveButton}
+              onClick={() => remove(tableId)}
+            >
+              삭제
+            </Button>
+          </ButtonGroup>
+        </Flex>
+        <ScheduleDndProvider>
+          <ScheduleTable
+            key={`schedule-table-${index}`}
+            schedules={schedules}
+            tableId={tableId}
+            onScheduleTimeClick={(timeInfo) =>
+              handleScheduleTimeClick(tableId, timeInfo)
+            }
+            onDeleteButtonClick={(timeInfo) =>
+              handleDeleteButtonClick(tableId, timeInfo)
+            }
+          />
+        </ScheduleDndProvider>
+      </Stack>
+    );
+  }
+);
 export const ScheduleTables = memo(() => {
   const { schedulesMap, setSchedulesMap } = useScheduleContext();
   const [searchInfo, setSearchInfo] = useState<{
@@ -65,46 +141,18 @@ export const ScheduleTables = memo(() => {
     <>
       <Flex w="full" gap={6} p={6} flexWrap="wrap">
         {Object.entries(schedulesMap).map(([tableId, schedules], index) => (
-          <Stack key={tableId} width="600px">
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading as="h3" fontSize="lg">
-                시간표 {index + 1}
-              </Heading>
-              <ButtonGroup size="sm" isAttached>
-                <Button
-                  colorScheme="green"
-                  onClick={() => setSearchInfo({ tableId })}
-                >
-                  시간표 추가
-                </Button>
-                <Button
-                  colorScheme="green"
-                  mx="1px"
-                  onClick={() => duplicate(tableId)}
-                >
-                  복제
-                </Button>
-                <Button
-                  colorScheme="green"
-                  isDisabled={disabledRemoveButton}
-                  onClick={() => remove(tableId)}
-                >
-                  삭제
-                </Button>
-              </ButtonGroup>
-            </Flex>
-            <ScheduleTable
-              key={`schedule-table-${index}`}
-              schedules={schedules}
-              tableId={tableId}
-              onScheduleTimeClick={(timeInfo) =>
-                handleScheduleTimeClick(tableId, timeInfo)
-              }
-              onDeleteButtonClick={(timeInfo) =>
-                handleDeleteButtonClick(tableId, timeInfo)
-              }
-            />
-          </Stack>
+          <ScheduleTableWrapper
+            key={tableId}
+            tableId={tableId}
+            schedules={schedules}
+            index={index}
+            setSearchInfo={setSearchInfo}
+            duplicate={duplicate}
+            disabledRemoveButton={disabledRemoveButton}
+            remove={remove}
+            handleScheduleTimeClick={handleScheduleTimeClick}
+            handleDeleteButtonClick={handleDeleteButtonClick}
+          />
         ))}
       </Flex>
       <SearchDialog searchInfo={searchInfo} onClose={handleSearchDialogClose} />
