2160f57f09fe1fe801d04adb663593eda83d9346
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-09-10 00:46:21 +0900

    feat: useAutoCallback 생성

---
+++ src/hooks/useAutoCallback.ts
@@ -0,0 +1,12 @@
+import { useCallback, useRef } from "react";
+
+type AnyFunction = (...args: any[]) => any;
+
+export const useAutoCallback = (callback: AnyFunction) => {
+  const ref = useRef(callback);
+  ref.current = callback;
+
+  return useCallback((...args: any[]) => {
+    return ref.current(...args);
+  }, []);
+};
